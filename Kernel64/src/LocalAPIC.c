084c840e2eb835d0a391113542c94b9407f092c2
Kangmin Park <l4stpr0gr4m@gmail.com>
2018-02-28 06:51:40 +0900

    Update Local APIC
    Update to send EOI to Local APIC, to set task priority register,
    to initialize local vector table.
    
    Signed-off-by: Kangmin Park <l4stpr0gr4m@gmail.com>

--- Kernel64/src/LocalAPIC.c
+++ Kernel64/src/LocalAPIC.c
@@ -16,3 +16,36 @@ void kEnableSoftwareLocalAPIC(void)
 	qwLocalAPICBaseAddress = kGetLocalAPICBaseAddress();
 	*(DWORD*)(qwLocalAPICBaseAddress + APIC_REGISTER_SVR) |= 0x100;
 }
+
+void kSendEOIToLocalAPIC(void)
+{
+	QWORD qwLocalAPICBaseAddress;
+
+	qwLocalAPICBaseAddress = kGetLocalAPICBaseAddress();
+	*(DWORD*)(qwLocalAPICBaseAddress + APIC_REGISTER_EOI) = 0;
+}
+
+void kSetTaskPriority(BYTE bPriority)
+{
+	QWORD qwLocalAPICBaseAddress;
+
+	qwLocalAPICBaseAddress = kGetLocalAPICBaseAddress();
+	*(DWORD*)(qwLocalAPICBaseAddress + APIC_REGISTER_TASKPRIORITY) = bPriority;
+}
+
+void kInitializeLocalVectorTable(void)
+{
+	QWORD qwLocalAPICBaseAddress;
+	DWORD dwTempValue;
+
+	qwLocalAPICBaseAddress = kGetLocalAPICBaseAddress();
+	*(DWORD*)(qwLocalAPICBaseAddress + APIC_REGISTER_TIMER) |= APIC_INTERRUPT_MASK;
+	*(DWORD*)(qwLocalAPICBaseAddress + APIC_REGISTER_LINT0) |= APIC_INTERRUPT_MASK;
+	*(DWORD*)(qwLocalAPICBaseAddress + APIC_REGISTER_LINT1) |= APIC_TRIGGERMODE_EDGE
+		| APIC_POLARITY_ACTIVEHIGH | APIC_DELIVERYMODE_NMI;
+	*(DWORD*)(qwLocalAPICBaseAddress + APIC_REGISTER_ERROR) |= APIC_INTERRUPT_MASK;
+	*(DWORD*)(qwLocalAPICBaseAddress + 
+			APIC_REGISTER_PERFORMANCEMONITORINGCOUNTER) |= APIC_INTERRUPT_MASK;
+	*(DWORD*)(qwLocalAPICBaseAddress + 
+			APIC_REGISTER_THERMALSENSOR) |= APIC_INTERRUPT_MASK;
+}
--- Kernel64/src/LocalAPIC.h
+++ Kernel64/src/LocalAPIC.h
@@ -41,7 +41,18 @@
 #define APIC_DESTINATIONSHORTHAND_ALLINCLUDINGSELF	0x080000
 #define APIC_DESTINATIONSHORTHAND_ALLEXCLUDINGSELF	0x0C0000
 
+#define APIC_INTERRUPT_MASK			0x010000
+
+#define APIC_TIMERMODE_PERIODIC		0x020000
+#define APIC_TIMERMODE_ONESHOT		0x000000
+
+#define APIC_POLARITY_ACTIVELOW		0x002000
+#define APIC_POLARITY_ACTIVEHIGH	0x000000
+
 QWORD kGetLocalAPICBaseAddress(void);
 void kEnableSoftwareLocalAPIC(void);
+void kSendEOIToLocalAPIC(void);
+void kSetTaskPriority(BYTE bPriority);
+void kInitializeLocalVectorTable(void);
 
 #endif
