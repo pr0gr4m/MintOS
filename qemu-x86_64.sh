28b4e4918884cfb937c91d9c398218482f64e1a3
Kangmin Park <l4stpr0gr4m@gmail.com>
2018-03-01 13:21:38 +0900

    Update Script To Test 16 Core

--- qemu-x86_64.sh
+++ qemu-x86_64.sh
@@ -1,7 +1,7 @@
 if [ $# == '0' ]; then
 	qemu-system-x86_64 -L . -m 64 -fda Disk.img -hda HDD.img -boot a -localtime -M pc -serial tcp::4444,server,nowait -smp 2
 elif [ $1 == "hdd" ]; then
-	qemu-system-x86_64 -L . -m 64 -fda Disk.img -hda HDD.img -boot a -localtime -M pc -serial tcp::4444,server,nowait -smp 2
+	qemu-system-x86_64 -L . -m 64 -fda Disk.img -hda HDD.img -boot a -localtime -M pc -serial tcp::4444,server,nowait -smp 16
 elif [ $1 == "rdd" ]; then
-	qemu-system-x86_64 -L . -m 64 -fda Disk.img -boot a -localtime -M pc -serial tcp::4444,server,nowait -smp 2
+	qemu-system-x86_64 -L . -m 64 -fda Disk.img -boot a -localtime -M pc -serial tcp::4444,server,nowait -smp 16
 fi
