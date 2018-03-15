b38265d7d39dc33d88fb1a4e5f57698959e49c15
Kangmin Park <l4stpr0gr4m@gmail.com>
2018-03-15 19:04:22 +0900

    Update Page To Divide Privilege
    Update page table to divide kernel space and user space.
    PML4 table and Page directory pointer table set US bit,
    kernel page directory table does not set US bit,
    user page directory table set US bit.
    
    Signed-off-by: Kangmin Park <l4stpr0gr4m@gmail.com>

--- Kernel32/src/Page.c
+++ Kernel32/src/Page.c
@@ -1,4 +1,8 @@
 #include "Page.h"
+#include "../../Kernel64/src/Task.h"
+
+#define DYNAMICMEMORY_START_ADDRESS		((TASK_STACKPOOLADDRESS + 0x1FFFFF) & \
+										0xFFE00000)
 
 void kInitializePageTables(void)
 {
@@ -6,13 +10,15 @@ void kInitializePageTables(void)
 	PDPTENTRY* pstPDPTEntry;
 	PDENTRY* pstPDEntry;
 	DWORD dwMappingAddress;
+	DWORD dwPageEntryFlags;
 	int i;
 
 	// create a PML4 Table
 	// init first entry with default value
 	// init others with 0x00
 	pstPML4TEntry = (PML4TENTRY*)0x100000;
-	kSetPageEntryData(&(pstPML4TEntry[0]), 0x00, 0x101000, PAGE_FLAGS_DEFAULT, 0);
+	kSetPageEntryData(&(pstPML4TEntry[0]), 0x00, 0x101000,
+			PAGE_FLAGS_DEFAULT | PAGE_FLAGS_US, 0);
 	for (i = 1; i < PAGE_MAXENTRYCOUNT; i++)
 	{
 		kSetPageEntryData(&(pstPML4TEntry[i]), 0, 0, 0, 0);
@@ -24,7 +30,7 @@ void kInitializePageTables(void)
 	for (i = 0; i < 64; i++)
 	{
 		kSetPageEntryData(&(pstPDPTEntry[i]), 0, 0x102000 + (i * PAGE_TABLESIZE),
-				PAGE_FLAGS_DEFAULT, 0);
+				PAGE_FLAGS_DEFAULT | PAGE_FLAGS_US, 0);
 	}
 	for (i = 64; i < PAGE_MAXENTRYCOUNT; i++)
 	{
@@ -37,8 +43,13 @@ void kInitializePageTables(void)
 	dwMappingAddress = 0;
 	for (i = 0; i < PAGE_MAXENTRYCOUNT * 64; i++)
 	{
+		if (i < ((DWORD)DYNAMICMEMORY_START_ADDRESS / PAGE_DEFAULTSIZE))
+			dwPageEntryFlags = PAGE_FLAGS_DEFAULT | PAGE_FLAGS_PS;		// kernel DPL
+		else
+			dwPageEntryFlags = PAGE_FLAGS_DEFAULT | PAGE_FLAGS_PS | PAGE_FLAGS_US;	// user DPL
+
 		kSetPageEntryData(&(pstPDEntry[i]), (i * (PAGE_DEFAULTSIZE >> 20)) >> 12,
-				dwMappingAddress, PAGE_FLAGS_DEFAULT | PAGE_FLAGS_PS, 0);
+				dwMappingAddress, dwPageEntryFlags, 0);
 		dwMappingAddress += PAGE_DEFAULTSIZE;
 	}
 }
